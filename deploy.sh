77d25906dcc226dd291267bddf4fdfb73ae53e47
Daniel Hillerström <daniel.hillerstrom@ed.ac.uk>
2024-05-08 16:52:25 +0200

    Update deployment script

--- deploy.sh
+++ deploy.sh
@@ -1,20 +1,19 @@
 #!/usr/bin/env bash
 
-git checkout inc
-git checkout origin/main -- inc/fiber.h
-git mv inc/fiber.h fiber.h
-git rm -rf inc
-git add fiber.h
-git commit -m "Deploy header file"
+git checkout inc &&
+git checkout origin/main -- inc/fiber.h &&
+git mv inc/fiber.h fiber.h &&
+git rm -rf inc &&
+git commit -m "Deploy header file" &&
 git push origin inc
 
-git checkout src
-git checkout origin/main -- src/asyncify
-git mv src/asyncify asyncify
-git checkout origin/main -- src/wasmfx
-git mv src/wasmfx wasmfx
-git rm -rf src
-git commit -m "Deploy implementation files"
+git checkout src &&
+git checkout origin/main -- src/asyncify &&
+git mv src/asyncify asyncify &&
+git checkout origin/main -- src/wasmfx &&
+git mv src/wasmfx wasmfx &&
+git rm -rf src &&
+git commit -m "Deploy implementation files" &&
 git push origin src
 
 git checkout main

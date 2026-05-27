350727e42663b90a8d9bb05589da631499878e55
yinamy <yinamy@outlook.com>
2026-05-27 13:18:52 +0100

    Refactored scheduler.c to look more like scheduler_switch.c, added output validation

--- examples/scheduler.c
+++ examples/scheduler.c
@@ -1,7 +1,13 @@
+// Benchmark that runs 10 fibers at the same time, each fiber increments 
+// a number. Intended as an itersum-analogue that exhibits what we get 
+// from using the switch instruction over resume/suspend in a green 
+// threads-like setting.
+
 #include <stdbool.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <assert.h>
 
 #include <fiber.h>
 
@@ -9,6 +15,10 @@
 #define PRINT_RESULTS 0
 #define NUM_WORKERS 10
 #define SWITCHES 10000000
+
+// Global state for scheduler
+bool keep_going = true;
+uint32_t next = 0;
 
 // Array of workers
 static fiber_t workers[NUM_WORKERS];
@@ -18,6 +28,38 @@ static bool worker_status[NUM_WORKERS];
 
 // Array of results
 static int32_t results[NUM_WORKERS];
+
+void update_state() {
+  // Update global variable `next` to point to the next available worker (if any).
+  uint32_t i = 0;
+  for (next = (next + 1) % NUM_WORKERS; i < NUM_WORKERS; ++i, next = (next+1) % NUM_WORKERS) {
+    if (!worker_status[next]) {
+      break;
+    }
+  }
+  // Update keep_going variable too
+  keep_going = i < NUM_WORKERS;
+}
+
+void scheduler() {
+  fiber_result_t status;
+  // Run next worker for one step.
+  do {
+    (void)fiber_resume(workers[next], &results[next], &status);
+    switch (status) {
+    case FIBER_OK:
+      worker_status[next] = true;
+      break;
+    case FIBER_YIELD:
+      break;
+    case FIBER_ERROR:
+      abort(); // A fiber should never enter the error state.
+      break;
+    }
+  // Find the next available worker and determine if we should keep going.
+  update_state();
+  } while (keep_going);
+}
 
 void* increment(void *arg) {
   int32_t *result = (int32_t*)(intptr_t)arg;
@@ -41,41 +83,18 @@ int main(void) {
     worker_status[i] = false;
   }
 
-  // Scheduler
-  bool keep_going = true;
-  uint32_t next = 0;
-  fiber_result_t status;
-  do {
-    (void)fiber_resume(workers[next], &results[next], &status);
-    switch (status) {
-    case FIBER_OK:
-      worker_status[next] = true;
-      break;
-    case FIBER_YIELD:
-      break;
-    case FIBER_ERROR:
-      abort(); // A fiber should never enter the error state.
-      break;
-    }
-
-    // Find the next available worker.
-    uint32_t i = 0;
-    for (next = (next + 1) % NUM_WORKERS; i < NUM_WORKERS; ++i, next = (next+1) % NUM_WORKERS) {
-      if (!worker_status[next]) {
-        break;
-      }
-    }
-    keep_going = i < NUM_WORKERS;
-  } while (keep_going);
+  // Call scheduler to start running workers.
+  scheduler();
 
   #if PRINT_RESULTS
   for (uint32_t i = 0; i < NUM_WORKERS; ++i) {
     printf("%d\n", results[i]);
   }
   #endif
-
-  // Clean up
+  
+  // Validate results and clean up
   for (uint32_t i = 0; i < NUM_WORKERS; ++i) {
+    assert(results[i] == SWITCHES);
     fiber_free(workers[i]);
   }
 
@@ -84,3 +103,4 @@ int main(void) {
 
 #undef PRINT_RESULTS
 #undef NUM_WORKERS
+#undef SWITCHES

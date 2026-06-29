51184698dc088841fb085b676ac5153d7110bd03
yinamy <yinamy@outlook.com>
2026-06-29 15:55:08 +0100

    Add params.h to factor out `#define`s in pi, pi_switch, scheduler, scheduler_switch

---
+++ examples/params.h
@@ -0,0 +1,16 @@
+#ifndef PARAMS_H
+#define PARAMS_H
+
+// Turn output on/off
+#define PRINT_RESULTS 0
+
+// Parameters for pi and pi_switch
+#define NUM_TASKS     1000
+#define BATCH_SIZE    20000U
+#define YIELDS        50U
+
+// Parameters for scheduler and scheduler_switch
+#define NUM_WORKERS   10
+#define SWITCHES      10000000
+
+#endif

8eaeee732281648d7ad10803f9ac55b646113110
Ezra e. k. Cooper <ezra.cooper@ed.ac.uk>
2026-07-14 16:45:38 +0100

    Move scheduler to the top of the ray.c file. I will need to call it from the ray routines themselves.

--- examples/ray.c
+++ examples/ray.c
@@ -14,6 +14,86 @@ static char BUFFER[4 * HEIGHT * WIDTH] = {0};
 
 #define export(NAME) __attribute__((export_name(NAME)))
 
+
+#define MAX_TASKS 800
+
+// Array of workers
+static fiber_t workers[MAX_TASKS];
+
+// Array of statuses
+static bool fiber_ready[MAX_TASKS];
+static bool fiber_allocated[MAX_TASKS];
+static void *fiber_arg[MAX_TASKS];
+
+void
+scheduler_init() {
+  fiber_init();
+  for (uint32_t i = 0; i < MAX_TASKS; ++i) {
+    fiber_ready[i] = false;
+  }
+}
+
+void
+scheduler_loop() {
+  bool keep_going = true;
+  uint32_t next = 0;
+  fiber_result_t status;
+  do {
+    if (!fiber_allocated[next] || !fiber_ready[next]) {
+      return;
+    }
+    (void)fiber_resume(workers[next], fiber_arg[next], &status);
+    fiber_arg[next] = NULL;  // The arg only gets passed to the starting function; clear it afterwards.
+    switch (status) {
+    case FIBER_OK:
+      fiber_ready[next] = false;
+      fiber_allocated[next] = false;
+      break;
+    case FIBER_YIELD:
+      fiber_ready[next] = true;
+      break;
+    case FIBER_ERROR:
+      abort(); // A fiber should never enter the error state.
+      break;
+    }
+
+    // Find a ready fiber.
+    // `next` tracks the identity of the fiber.
+    // i counts the number of fibers checked; since `next` wraps around the array,
+    // we use i to ensure we just check each fiber once.
+    uint32_t i = 0;
+    for (next = (next + 1) % MAX_TASKS; i < MAX_TASKS; ++i, next = (next+1) % MAX_TASKS) {
+      if (fiber_allocated[next] && fiber_ready[next]) {
+        break;
+      }
+    }
+    keep_going = i < MAX_TASKS;
+  } while (keep_going);
+}
+
+void
+scheduler_finalize() {
+  for (uint32_t i = 0; i < MAX_TASKS; ++i) {
+    fiber_free(workers[i]);
+  }
+
+  fiber_finalize();
+}
+
+void
+scheduler_spawn(fiber_entry_point_t func, void *arg) {
+  for (uint32_t id = 0; id < MAX_TASKS; ++id) {
+      if (!fiber_allocated[id]) {
+          workers[id] = fiber_alloc(func);
+          fiber_ready[id] = true;
+          fiber_allocated[id] = true;
+          fiber_arg[id] = arg;
+          return;
+      }
+  }
+  abort(); // No available fiber slots.
+}
+
 struct pt {
     float x, y, z;
 };
@@ -163,85 +243,6 @@ render_stub(void *arg) {
     int time = (int)(intptr_t)arg;
     render(time);
     return NULL;
-}
-
-#define MAX_TASKS 800
-
-// Array of workers
-static fiber_t workers[MAX_TASKS];
-
-// Array of statuses
-static bool fiber_ready[MAX_TASKS];
-static bool fiber_allocated[MAX_TASKS];
-static void *fiber_arg[MAX_TASKS];
-
-void
-scheduler_init() {
-  fiber_init();
-  for (uint32_t i = 0; i < MAX_TASKS; ++i) {
-    fiber_ready[i] = false;
-  }
-}
-
-void
-scheduler_loop() {
-  bool keep_going = true;
-  uint32_t next = 0;
-  fiber_result_t status;
-  do {
-    if (!fiber_allocated[next] || !fiber_ready[next]) {
-      return;
-    }
-    (void)fiber_resume(workers[next], fiber_arg[next], &status);
-    fiber_arg[next] = NULL;  // The arg only gets passed to the starting function; clear it afterwards.
-    switch (status) {
-    case FIBER_OK:
-      fiber_ready[next] = false;
-      fiber_allocated[next] = false;
-      break;
-    case FIBER_YIELD:
-      fiber_ready[next] = true;
-      break;
-    case FIBER_ERROR:
-      abort(); // A fiber should never enter the error state.
-      break;
-    }
-
-    // Find a ready fiber.
-    // `next` tracks the identity of the fiber.
-    // i counts the number of fibers checked; since `next` wraps around the array,
-    // we use i to ensure we just check each fiber once.
-    uint32_t i = 0;
-    for (next = (next + 1) % MAX_TASKS; i < MAX_TASKS; ++i, next = (next+1) % MAX_TASKS) {
-      if (fiber_allocated[next] && fiber_ready[next]) {
-        break;
-      }
-    }
-    keep_going = i < MAX_TASKS;
-  } while (keep_going);
-}
-
-void
-scheduler_finalize() {
-  for (uint32_t i = 0; i < MAX_TASKS; ++i) {
-    fiber_free(workers[i]);
-  }
-
-  fiber_finalize();
-}
-
-void
-scheduler_spawn(fiber_entry_point_t func, void *arg) {
-  for (uint32_t id = 0; id < MAX_TASKS; ++id) {
-      if (!fiber_allocated[id]) {
-          workers[id] = fiber_alloc(func);
-          fiber_ready[id] = true;
-          fiber_allocated[id] = true;
-          fiber_arg[id] = arg;
-          return;
-      }
-  }
-  abort(); // No available fiber slots.
 }
 
 export("render_main")
